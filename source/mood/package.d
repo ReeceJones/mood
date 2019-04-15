1ae37ce82cd4fe533c25f4404a5e25f210d01b86
Reece Jones <reece_jones@icloud.com>
2019-04-15 08:23:32 -0700

    Add templates for easier html code generation

---
+++ source/mood/package.d
@@ -0,0 +1,8 @@
+module mood;
+
+public import mood.compiler;
+public import mood.node;
+public import mood.parser;
+public import mood.server;
+public import mood.vibe;
+public import mood.templates;
