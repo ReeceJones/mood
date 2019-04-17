4908ce574c52babfdcb5f7ef7d329e6029e5a66c
Reece Jones <reece_jones@icloud.com>
2019-04-17 12:07:53 -0700

    cleanup hello world example

--- examples/hello-world/source/app.d
+++ examples/hello-world/source/app.d
@@ -6,12 +6,9 @@ import mood.hook;
 
 shared static this()
 {
-	int foo = 1337;
-
 	auto router = new URLRouter;
 
-	router.get("/", moodRender!("index.html", foo));
-	router.get("/foo", moodRender!"foo.html");
+	router.get("/", moodRender!"index.html");
 	auto settings = new HTTPServerSettings;
 	settings.port = 9001;
 	settings.bindAddresses = ["::1", "0.0.0.0"];
